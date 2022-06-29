2cb61fadac47d4cede6a059a27a9f5c9c016d5dd
Kevin McIntosh <kmac303@gmail.com>
2022-06-28 18:04:25 -0600

    commit tree 3e510672027a58b161030918e2b3f4a248d293e2

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(driversNames, string) {
+    return driversNames.filter(
+        matchingDrivers => (matchingDrivers.toLowerCase() === string.toLowerCase())
+    );
+  }
+
+  function fuzzyMatch(driversNames, string) {
+    return driversNames.filter(
+        matchingDrivers => (matchingDrivers.toLowerCase().indexOf(string.toLowerCase())) === 0
+    );
+  }
+
+  function matchName(driverObjects, string) {
+    return driverObjects.filter((record) => record.name === string)
+  }
